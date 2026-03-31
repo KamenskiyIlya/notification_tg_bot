1c1fc032037338a4cab22e6e0229583b7e7bdf21
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-03-31 23:00:56 +0300

    Подгрузил дописанный код

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from environs import env
-import requests
 from pprint import pprint
 
-
-env.read_env()
-
-DEVMAN_TOKEN = env('DEVMAN_TOKEN')
-BOT_TOKEN = env('TG_BOT_TOKEN')
+from environs import env
+import requests
+import telegram
 
 
-def checks():
+def check():
 	url = 'https://dvmn.org/api/user_reviews/'
 	headers = {
 		'Authorization': f'Token {DEVMAN_TOKEN}'
@@ -19,39 +15,83 @@ def checks():
 	response_payload = response.json()
 	return response_payload
 
-def long_pooling_checks(timestamp=None):
+def long_pooling_check(timestamp=None):
 	url = 'https://dvmn.org/api/long_polling/'
 	headers = {
 		'Authorization': f'Token {DEVMAN_TOKEN}'
 	}
+	params = {}
 
 	if timestamp:
-		params = {
-			'timestamp': timestamp,
-		}
-		response = requests.get(url, headers=headers, params=params)
-	else:
-		response = requests.get(url, headers=headers)
+		params['timestamp'] = timestamp
 
+	response = requests.get(url, headers=headers, params=params)
 	response.raise_for_status()
 	response_payload = response.json()
 
 	if response_payload['status'] == 'timeout':
-		timestamp = response_payload['timestamp_to_request']
+		new_timestamp = response_payload['timestamp_to_request']
+		return None, new_timestamp
 	elif response_payload['status'] == 'found':
-		timestamp = response_payload['last_attempt_timestamp']
+		new_timestamp = response_payload['last_attempt_timestamp']
 
 	return response_payload, timestamp
 
 
+def get_chat_info(bot, chat_id):
+	try:
+		chat = bot.get_chat(chat_id)
+		if chat.first_name:
+			user_name = chat.first_name
+			return user_name
+		return 'пользователь'
+	except Exception as e:
+		print(f'Не удалось получить имя пользователя.\nОшибка: {e}')
+		return 'пользователь'
+
+
+def send_notification(bot, chat_id, name, text):
+	try:
+		bot.send_message(chat_id=chat_id, text=text)
+	except telegram.error.TimedOut as e:
+		print(f'Вышло время ожидания ответа от telegram.\nОшибка: {e}')
+
+
 if __name__ == '__main__':
-	result, timestamp = long_pooling_checks()
-	pprint(result)
+	env.read_env()
+	DEVMAN_TOKEN = env('DEVMAN_TOKEN')
+	BOT_TOKEN = env('TG_BOT_TOKEN')
+	CHAT_ID = env('CHAT_ID')
+	bot = telegram.Bot(token=BOT_TOKEN)
+	timestamp = None
+
+	user_name = get_chat_info(bot, CHAT_ID)
+
 	while True:
 		try:
-			result, timestamp = long_pooling_checks(timestamp)
-			pprint(result)
+			result, timestamp = long_pooling_check(timestamp)
 		except requests.exceptions.ReadTimeout as e:
 			print(f'Сервер не ответил, ошибка:\n {e}')
 		except requests.exceptions.ConnectionError as e:
 			print(f'Отсутствует подключение к сети, ошибка:\n {e}')
+
+		if result:
+			attempt_inf = result['new_attempts'][0]
+			lesson_title = attempt_inf['lesson_title']
+			lesson_url = attempt_inf['lesson_url']
+			desicion = not attempt_inf['is_negative']
+			text = (
+				f'{user_name}, у Вас проверили работу\n'
+				f'По уроку: {lesson_title}\n'
+				f'Ссылка на урок: {lesson_url}\n\n'
+			)
+
+			if desicion is False:
+				text += 'Результат: В работе нашлись ошибки, необходимы правки.'
+			else:
+				text += (
+					'Результат: Работа принята,'
+					'можно переходить к следующему уроку.'
+				)
+
+			send_notification(bot, CHAT_ID, user_name, text)
