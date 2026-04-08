cec5a45c0b2dc5727ebd868de6dfd868a728b038
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-04-08 21:41:32 +0300

    Настроил логирование бота

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
 from time import sleep
+import logging
+from logging.handlers import RotatingFileHandler
 
 from environs import env
 import requests
 import telegram
 
+
+class TelegramLogsHandler(logging.Handler):
+    
+    def __init__(self, bot_token, chat_id):
+        super().__init__()
+        self.bot = telegram.Bot(token=bot_token)
+        self.chat_id = chat_id
+    
+    def emit(self, record):
+        try:
+            log_entry = self.format(record)
+            self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+        except:
+            pass
+        
+
+def configuration_logger(bot_token, chat_id):
+    logger = logging.getLogger('tg_bot_logger')
+    logger.setLevel(logging.DEBUG)
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(filename)s: %(levelname)s: %(message)s '
+    )
+    
+    file_handler = RotatingFileHandler(
+        'tg_bot.log',
+        maxBytes= 10 * 1024 * 1024,
+        backupCount=3,
+        encoding='utf-8'        
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler
+    
+    tg_handler = TelegramLogsHandler(bot_token, chat_id)
+    tg_handler.setFormatter(formatter)
+    logger.addHandler(tg_handler)
+    
+    return logger
 
 def get_latest_checks(devman_token):
     url = 'https://dvmn.org/api/user_reviews/'
@@ -68,10 +108,17 @@ def main():
     env.read_env()
     DEVMAN_TOKEN = env('DEVMAN_TOKEN')
     BOT_TOKEN = env('TG_BOT_TOKEN')
+    LOG_BOT_TOKEN = env('TG_BOT_TOKEN')
     CHAT_ID = env('CHAT_ID')
+    ADMIN_CHAT_ID = env('ADMIN_CHAT_ID')
     REQUESTS_DELAY = env.int('REQUESTS_DELAY', default=600)
+    
+    logger = configuration_logger(LOG_BOT_TOKEN, ADMIN_CHAT_ID)
+
     bot = telegram.Bot(token=BOT_TOKEN)
     timestamp = None
+    
+    logger.info('Бот запущен')
 
     while True:
         for attempt in range(3):
@@ -85,14 +132,16 @@ def main():
                     text = generate_notification_text(devman_response)
                     bot.send_message(chat_id=CHAT_ID, text=text)
             except telegram.error.TimedOut as e:
-                print(
-                    'Вышло время ожидания ответа от telegram.\n'
-                    f'Ошибка: {e}'
+                logger.warning(
+                    'Вышло время ожидания ответа от telegram. '
+                    f'Ошибка: {e}.'
                 )
             except requests.exceptions.ReadTimeout:
-                pass
+                logger.warning('Вышло время ожидания ответа от сайта.')
             except requests.exceptions.ConnectionError as e:
-                print(f'Отсутствует подключение к сети, ошибка:\n {e}')
+                logger.error(f'Отсутствует подключение к сети, ошибка: {e}')
+            except Exception:
+                logger.exception('Произошла неожиданная ошибка')
 
         sleep(REQUESTS_DELAY)
 
